bbf9540837bf0323856c58add51637bf2a51d146
jts3444 <jts3444@hotmail.com>
2018-11-10 14:46:46 -0800

    Fixed issue with add.html, fixed typo in Category Controller, finished part 1

--- src/main/java/org/launchcode/models/Category.java
+++ src/main/java/org/launchcode/models/Category.java
@@ -22,4 +22,16 @@ public class Category {
     public Category(String name) {
         this.name = name;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
